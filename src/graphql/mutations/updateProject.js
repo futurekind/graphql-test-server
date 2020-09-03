369fa86919de75db81bd712d2459e2cbca406098
Fabian Trinler <fabian.trinler@blateral.com>
2020-09-03 15:18:25 +0200

    Removing node_modules

--- src/graphql/mutations/updateProject.js
+++
@@ -1,7 +0,0 @@
-const getDB = require('../../db');
-
-module.exports = (_, { project: { id, title } }) => {
-    const p = getDB().get('projects').find({ id }).assign({ title }).write();
-
-    return p;
-};
